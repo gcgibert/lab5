d35a1a175b34963525bc43e474ffb3b3f0d19132
unknown <gerard.carramona@gmail.com>
2021-06-03 16:14:56 +0100

    Task1 done.

---
+++ task1/src/main/java/pt/ual/pp/lab5/App.java
@@ -0,0 +1,27 @@
+package pt.ual.pp.lab5;
+
+interface I {
+    void m();
+}
+
+class A implements I {
+    @Override
+    public void m() {
+        System.out.println("m implemented in A");
+    }
+}
+
+class B {
+    public void goal(I impl) {
+        System.out.print("B is executing goal: ");
+        impl.m();
+    }
+}
+
+public class App {
+    public static void main(String[] args) {
+        B b = new B();
+        A a = new A();
+        b.goal(() -> System.out.println("m implemented lambda"));
+    }
+}
